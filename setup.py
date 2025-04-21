876d8f557b933933e1801403bda664cc95c036cc
Abdul Rafey <abdulrafey38@gmail.com>
2025-04-21 16:11:28 +0500

    chore: bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='Shiftify',
-    version='0.1.3',
+    version='0.1.4',
     packages=find_packages(),
     long_description=long_description,
     long_description_content_type='text/markdown',
